94cd8dd1bc0059572951b76c38484218095c2461
KOREAparksh <tmdgks2222@naver.com>
2022-03-27 18:32:28 +0900

    Feat: 새 예약 페이지 구현

---
+++ front/src/component/test.js
@@ -0,0 +1,15 @@
+function Test1()
+{
+  return(
+    <a
+    className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+      test
+    </a>
+  );
+}
+
+export default Test1;
---
+++ front/src/pages/login.js
@@ -0,0 +1,12 @@
+import Test1 from "../component/test";
+
+function Login()
+{
+	return(
+	<Test1>
+
+	</Test1>
+	);
+}
+
+export default Login;
