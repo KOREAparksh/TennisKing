229fee7de5eb34d4319ca0743c7d51705814c4b6
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-03-27 22:15:28 +0900

    divide router

--- back/src/routers/index.js
+++ back/src/routers/index.js
@@ -5,7 +5,7 @@ const rentRouter = require("./rent");
 const reserveRouter = require("./reserves");
 
 router.use("/places", placeRouter);
-router.use("/rents", rentRouter);
 router.use("/reserves", reserveRouter);
+router.use("/rent", rentRouter);
 
 module.exports = router;
