c3c88a6085da40f66fd2e26c96b28ca3d0bf0c97
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-03-28 14:08:41 +0900

    ⚡️ Execute reservation

--- back/src/index.js
+++ back/src/index.js
@@ -15,6 +15,7 @@ sequelize
     });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
 app.listen(port, () => {
---
+++ back/src/modules/login.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const qs = require("qs");
+require("dotenv").config();
+
+const getSessionId = async () => {
+    const loginData = {
+        process_check: "login",
+        ret_url: "",
+        memid: process.env.NYJ_USERNAME,
+        mempw: process.env.NYJ_PASSWORD,
+    };
+    const loginOptions = {
+        method: "POST",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: qs.stringify(loginData),
+        url: "https://www.nyj.go.kr/rent/member/process",
+    };
+
+    const cookies = (await axios(loginOptions)).headers["set-cookie"];
+    return cookies[2].split(" ")[0];
+};
+
+module.exports = getSessionId;
