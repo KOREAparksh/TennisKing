55ee129229cd6fc336cc1f95f36958339f3314d5
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-03-30 05:10:22 +0900

    예약 큐 테이블 상태관리

--- back/src/routers/rent.js
+++ back/src/routers/rent.js
@@ -28,16 +28,28 @@ router.get(
         Promise.allSettled(
             reserves.ReserveTimes.map(async (value) => {
                 try {
-                    const start = new Date();
-                    const sessionId = await getSessionId();
-                    const rentData = await getRentData(
-                        sessionId,
-                        reserves.Place.dataValues,
-                        value.dataValues,
-                        reserves.dataValues.member
-                    );
+                    if (value.dataValues.status !== 1) {
+                        const start = new Date();
+                        const sessionId = await getSessionId();
+                        const rentData = await getRentData(
+                            sessionId,
+                            reserves.Place.dataValues,
+                            value.dataValues,
+                            reserves.dataValues.member
+                        );
 
-                    await executeRent(value.dataValues.id, sessionId, rentData, start);
+                        await executeRent(value.dataValues.id, sessionId, rentData, start);
+
+                        const reserves = await Reserve.findOne({
+                            include: [{ model: Place }, { model: ReserveTime }],
+                            where: { id: reserveId },
+                        });
+                        if (reserves.reserveTimes.filter((value) => value.status !== 1).length === 0) {
+                            await Reserve.update({ status: 1 }, { where: { id: reserveId } });
+                        } else {
+                            await Reserve.update({ status: 2 }, { where: { id: reserveId } });
+                        }
+                    }
                 } catch (err) {
                     logger.reservationFail(
                         `ReserveId: ${reserves.id}, PlaceId: ${reserves.Place.id}, ReserveTimeId: ${value.dataValues.id}\n${err}`
