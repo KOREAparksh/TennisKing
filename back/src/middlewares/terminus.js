f83b93c3cc95138ee74cbb206de5c81a4c078239
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-03-30 06:11:51 +0900

    cookie secure delete

--- back/src/middlewares/terminus.js
+++ back/src/middlewares/terminus.js
@@ -5,8 +5,8 @@ const terminus = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next))
         .then((result) => {
             if (req.headers.changed) {
-                res.cookie("access", JWT.accessSign(), { httpOnly: true, secure: true, sameSite: "none" });
-                res.cookie("refresh", JWT.refreshSign(), { httpOnly: true, secure: true, sameSite: "none" });
+                res.cookie("access", JWT.accessSign(), { httpOnly: true, sameSite: "none" });
+                res.cookie("refresh", JWT.refreshSign(), { httpOnly: true, sameSite: "none" });
             }
             res.status(httpStatus.OK).json(result);
         })
