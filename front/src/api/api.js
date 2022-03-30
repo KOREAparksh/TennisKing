014e33ad7691cf4f497f70c08a1db72b327de17c
KOREAparksh <tmdgks2222@naver.com>
2022-03-30 10:29:26 +0900

    Feat: 쿠키 로직 작성 but 확인못함

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,12 +1,30 @@
 import axios from "axios";
+import { getCookie, setCookie } from "./cookie";
 
 const client = axios.create({
   baseURL: `${process.env.REACT_APP_API_ENDPOINT}/api`,
   withCredentials: true,
 });
 
+client.interceptors.request.use(
+  function (config){
+    config.headers['access'] = getCookie('access');
+    config.headers['refresh'] = getCookie('refresh');
+    return config;
+  },
+  function (err) {
+    return Promise.reject(err);
+  }
+);
+
 client.interceptors.response.use(
-  (res) => res,
+  function (res) {
+    console.log(res.headers);
+    console.log(res.headers['refresh']);
+    setCookie('access', res.headers['access'], ["httpOnly", "sameSite"]);
+    setCookie('refresh', res.headers['refresh'], ["httpOnly", "sameSite"]);
+    return res;
+  },
   (err) => {
     if (
       err.response.status === 401 &&
