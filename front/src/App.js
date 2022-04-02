8aaac02084e2d7b138c45d7afc39b2945ca10f9e
KOREAparksh <tmdgks2222@naver.com>
2022-04-02 19:06:25 +0900

    bootstrap install

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login/Login';
 import Main from './pages/main/Main';
 import Reservation from './pages/reservations/Reservation';
 import {Route, Routes} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
--- front/src/pages/login/Login.js
+++ front/src/pages/login/Login.js
@@ -9,52 +9,47 @@ function Login(props) {
   const navigate = useNavigate();
 
   const onChangeText = (object) => {
-    setText(object.target.value);
+	setText(object.target.value);
   };
 
   const onSubmit = async (e) => {
-    e.preventDefault();
-    if (text.length <= 0) return false;
-    try {
-      await api.login(text);
-      navigate("/");
-    } catch (err) {
-      if (err.response.data.message === "Invalid Code") {
-        setIsValid(false);
-      }
-    }
-    return false;
+	e.preventDefault();
+	if (text.length <= 0) return false;
+	try {
+	  await api.login(text);
+	  navigate("/");
+	} catch (err) {
+	  if (err.response.data.message === "Invalid Code") {
+		setIsValid(false);
+	  }
+	}
+	return false;
   };
 
   return (
-    <div id="Container">
-      <div id="Logo">☆환 서완우 전무님 영☆</div>
-      <div id="Test">
-        <form onSubmit={onSubmit}>
-          <div>
-            <input
-              type="text"
-              id="InputCode"
-              placeholder="코드를 입력하세요"
-              value={text}
-              onChange={onChangeText}
-            />
-          </div>
-          <div id="WrongCode" hidden={isValid}>
-            코드가 정확하지 않습니다.
-          </div>
-          <div>
-            <input
-              type="button"
-              id="LoginButton"
-              value="입장"
-              onClick={onSubmit}
-            />
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	<div id="Container">
+	  <div id="Logo">☆환 서완우 전무님 영☆</div>
+	  <div id="Test">
+		<form onSubmit={onSubmit}>
+		  <div>
+			<input
+			  type="text"
+			  id="InputCode"
+			  placeholder="코드를 입력하세요"
+			  value={text}
+			  onChange={onChangeText}
+			/>
+		</div>
+		<div id="WrongCode" hidden={isValid}>
+			코드가 정확하지 않습니다.
+		  </div>
+		  <div>
+			<button type="button" class="btn btn-default" onClick={onSubmit}>로그인</button>
+		</div>
+		</form>
+	</div>
+	</div>
+);
 }
 
 export default Login;
